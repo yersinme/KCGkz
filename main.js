a435355d031fb18d9480ce3a20f950c1c4a6b29b
yersinme <161592534+yersinme@users.noreply.github.com>
2024-04-25 14:46:53 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,48 +1,63 @@
-function toggleMenu() {
-    const links = document.querySelector('.header_links');
+// function toggleMenu() { 
+//     const links = document.querySelector('.header_links'); 
+ 
+//     links.classList.toggle('show-on-mobile'); 
+// }; 
+ 
+// function callNumber() { 
+//     const phoneNumber = '+77787463362'; 
+//     alert('Номер для звонка: ' + phoneNumber); 
+// }; 
+ 
+function openWhatsApp() { 
+    const phoneNumber = '+77788968934'; 
+    const url = 'https://wa.me/' + phoneNumber; 
+    window.open(url, '_blank'); 
+}; 
+ 
+function openInstagram() { 
+    const username = 'kazcg.kz'; 
+    const url = 'https://www.instagram.com/' + username; 
+    window.open(url, '_blank'); 
+}; 
+ 
+let opened = false; 
+ 
+function toggleBurger(element) { 
+  opened = !opened; 
+  element.classList.toggle('openedBurger', opened); 
+  const headerLinks = document.querySelector('.header_links'); 
+  const header_btns = document.querySelector('.header_btns'); 
 
-    links.classList.toggle('show-on-mobile');
-};
+  headerLinks.style.display = opened ? 'flex' : 'none';
+  header_btns.style.display = opened ? 'none' : 'flex';
 
-function callNumber() {
-    const phoneNumber = '+77787463362';
-    alert('Номер для звонка: ' + phoneNumber);
-};
-
-function openWhatsApp() {
-    const phoneNumber = '+77788968934';
-    const url = 'https://wa.me/' + phoneNumber;
-    window.open(url, '_blank');
-};
-
-function openInstagram() {
-    const username = 'kazcg.kz';
-    const url = 'https://www.instagram.com/' + username;
-    window.open(url, '_blank');
-};
-
-let lastScrollTop = 0;
-
-window.addEventListener("scroll", function() {
-    const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
-    if (currentScroll > lastScrollTop) {
-        // Скроллим вниз
-        document.querySelector('header').classList.add("header_hidden");
-    } else {
-        // Скроллим вверх
-        document.querySelector('header').classList.remove("header_hidden");
-    }
-    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
-}, false);
-
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        const targetId = this.getAttribute('href').substr(1);
-
-        document.getElementById(targetId).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
+  headerLinks.style.flexDirection = opened ? 'column' : 'row';  // Показываем или скрываем ссылки при открытии/закрытии бургера 
+} 
+ 
+ 
+let lastScrollTop = 0; 
+ 
+window.addEventListener("scroll", function() { 
+    const currentScroll = window.pageYOffset || document.documentElement.scrollTop; 
+    if (currentScroll > lastScrollTop) { 
+        // Скроллим вниз 
+        document.querySelector('header').classList.add("header_hidden"); 
+    } else { 
+        // Скроллим вверх 
+        document.querySelector('header').classList.remove("header_hidden"); 
+    } 
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; 
+}, false); 
+ 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => { 
+    anchor.addEventListener('click', function (e) { 
+        e.preventDefault(); 
+ 
+        const targetId = this.getAttribute('href').substr(1); 
+ 
+        document.getElementById(targetId).scrollIntoView({ 
+            behavior: 'smooth' 
+        }); 
+    }); 
 });
